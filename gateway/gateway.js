6e07d0c5f2773f5e32da903ca44ea6db56da09e8
Luca Keidel <info@lucakeidel.de>
2018-01-19 15:39:41 +0100

    Measure humidity

--- gateway/gateway.js
+++ gateway/gateway.js
@@ -5,12 +5,13 @@ const EVT = require('evrythng');
 var app = new EVT.App(process.env.EVRYTHNG_API_KEY);
 
 setInterval(() => { 
-    var ls = spawnSync( '/home/pi/ccn/build/bin/ccn-lite-peek /p/temp | /home/pi/ccn/build/bin/ccn-lite-pktdump -f 2', [], (error, stdout, stderr) => {
+    var ls = spawnSync( '/home/pi/ccn/build/bin/ccn-lite-peek /p/R2/hum | /home/pi/ccn/build/bin/ccn-lite-pktdump -f 2', [], (error, stdout, stderr) => {
         if (!error) {
-            console.log('[Update]: Set temperature to', stdout);
-            app.product('UnXb9FxxBXPwtpRaagdXfdCb').property().update({
-                temperature: stdout
+            console.log('[Update]: Set humditiy to', stdout);
+            app.product(process.env.EVRYTHNG_PRODUCT_ID).property().update({
+                humidity: stdout
             });
         }
     });
+
 }, 1000);
--- setup-ccn.sh
+++ setup-ccn.sh
@@ -2,7 +2,7 @@
 
 #CCNL_BASE="/home/pi/ccn/build/bin"
 CCNL_BASE=${CCNL_BASE:-"/home/pi/ccn/build/bin"}
-MAC_NODE="d8:80:39:02:c0:f9"
+MAC_NODE="ff:ff:ff:ff:ff:ff"
 MAC_WIRELESS=""
 PREFIX="/p"
 PREFIX_WIRELESS="/w"
@@ -17,11 +17,15 @@ pkill ccn-lite-relay
 
 $CCNL_BASE/ccn-lite-relay -e eth0 -w wpan0 > /dev/null 2>&1 &
 
-sleep 2
+sleep 5
+
+echo "Eth Face"
 
 FACEID=`$CCNL_BASE/ccn-lite-ctrl newETHface any $MAC_NODE 2049 | $CCNL_BASE/ccn-lite-ccnb2xml | grep FACEID | sed -e 's/^[^0-9]*\([0-9]\+\).*/\1/'`
 
 $CCNL_BASE/ccn-lite-ctrl prefixreg $PREFIX $FACEID
+
+echo "WPAN Face"
 
 FACEID_WIRELESS=`$CCNL_BASE/ccn-lite-ctrl newWPANface ff:ff:ff:ff:ff:ff:ff:ff 0x23 | $CCNL_BASE/ccn-lite-ccnb2xml | grep FACEID | sed -e 's/^[^0-9]*\([0-9]\+\).*/\1/'`
 
