53bab5abd410c89912d3377df5f6deec7d2cc39c
David Bohn <davbohn@googlemail.com>
2018-01-19 13:36:54 +0100

    Added setup script for pi

--- setup-ccn.sh
+++ setup-ccn.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 
-CCNL_BASE="/home/pi/ccn/build/bin"
+#CCNL_BASE="/home/pi/ccn/build/bin"
+CCNL_BASE=${CCNL_BASE:-"/home/pi/ccn/build/bin"}
 MAC_NODE="d8:80:39:02:c0:f9"
 PREFIX="/p"
 
@@ -11,6 +12,6 @@ fi
 
 $CCNL_BASE/ccn-lite-relay -e eth0 -v trace &
 
-FACEID = $CCNL_BASE/ccn-lite-ctrl newETHface any $MAC_NODE 2049 | $CCNL_BASE/ccn-lite-ccnb2xml | grep FACEID | sed -e 's/^[^0-9]*\([0-9]\+\).*/\1/'
+FACEID=`$CCNL_BASE/ccn-lite-ctrl newETHface any $MAC_NODE 2049 | $CCNL_BASE/ccn-lite-ccnb2xml | grep FACEID | sed -e 's/^[^0-9]*\([0-9]\+\).*/\1/'`
 
 $CCNL_BASE/ccn-lite-ctrl prefixreg $PREFIX $FACEID
