8e057f00efbb7cca065b0a61d449899b6013161e
David Bohn <davbohn@googlemail.com>
2018-01-19 13:33:13 +0100

    Added relay demo and pi setup

---
+++ setup-ccn.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+
+CCNL_BASE="/home/pi/ccn/build/bin"
+MAC_NODE="d8:80:39:02:c0:f9"
+PREFIX="/p"
+
+if [ "$EUID" -ne 0 ]
+    then echo "Please run as root, you noob"
+    exit
+fi
+
+$CCNL_BASE/ccn-lite-relay -e eth0 -v trace &
+
+FACEID = $CCNL_BASE/ccn-lite-ctrl newETHface any $MAC_NODE 2049 | $CCNL_BASE/ccn-lite-ccnb2xml | grep FACEID | sed -e 's/^[^0-9]*\([0-9]\+\).*/\1/'
+
+$CCNL_BASE/ccn-lite-ctrl prefixreg $PREFIX $FACEID
